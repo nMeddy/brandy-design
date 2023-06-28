57697d550a1f8b6b15242072ffda184f2fddff9a
Mister-no-body <leonsnguela08@gmail.com>
2023-06-28 23:38:26 +0100

    Mise en place du routage de l'App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
 import { React } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-import Footer from "./components/Footer/Footer";
 
 import "./App.css";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route index element={<Header />} />
-        <Route index element={<Footer />} />
-      </Routes>
+    <Routes>
+      <Route index element={<Header/>}>
+        
+      </Route>
+    </Routes>
+      
     </>
   );
 }
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import {Link, Outlet} from "react-router-dom"
+
+
+export default function Header(){
+    return (
+        <>
+            <h1>Hello</h1>
+        </>
+    )
+}
