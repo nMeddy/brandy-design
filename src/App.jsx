b0303701e025e41e2722985c68ca0d91a1563203
Mister-no-body <leonsnguela08@gmail.com>
2023-06-29 00:13:40 +0100

    mise en place de la navBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
 import { React } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
-
 import "./App.css";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route index element={<Header/>}>
-        
-      </Route>
-    </Routes>
-      
+      <Routes>
+        <Route index element={<Header />}></Route>
+      </Routes>
     </>
   );
 }
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,21 @@
-import {Link, Outlet} from "react-router-dom"
+import { Link, Outlet } from "react-router-dom";
+import './Header.css';
 
+export default function Header() {
+  return (
+    <>
+      <nav className="navBar">
+        <div className="nav-brand">
+          <h1 className="brand">Brandy design</h1>
+        </div>
 
-export default function Header(){
-    return (
-        <>
-            <h1>Hello</h1>
-        </>
-    )
+        <div className="nav-link">
+            <Link to='/'>Acceuil</Link>
+            <Link to='/'>Service</Link>
+            <Link to='/'>À propos</Link>
+            <Outlet/>
+        </div>
+      </nav>
+    </>
+  );
 }
