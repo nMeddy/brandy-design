02432a66d93a3cd1d877d2b6ef947da28f169880
Dash_NZITA <nzitapuindi2@gmail.com>
2023-07-04 01:19:13 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { React } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route index element={<Header />}></Route>
       </Routes>
+      <Footer/>
     </>
   );
 }
