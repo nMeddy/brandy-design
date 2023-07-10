f66c9e005570122ad992f777ee87b98914459845
Mister-no-body <leonsnguela08@gmail.com>
2023-07-11 00:33:52 +0100

    Code Refactorization

---
+++ src/components/LastPart/LastPart.jsx
@@ -0,0 +1,16 @@
+import "./LastPart.css";
+
+export default function LastPart() {
+  return (
+    <>
+      <div className="LastPart">
+        <div className="left">
+            <h1>Gauche</h1>
+        </div>
+        <div className="right">
+          <h1>LastPart</h1>
+        </div>
+      </div>
+    </>
+  );
+}
