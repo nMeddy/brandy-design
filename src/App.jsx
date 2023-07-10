f7e6e05d0563c7ff3b3128405028c76b11ce0f5d
Dash_NZITA <nzitapuindi2@gmail.com>
2023-07-10 23:12:57 +0200

    home end

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import { React } from "react";
 import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
 import "./App.css";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route index element={<Header />}></Route>
+        <Route index element={<Home />}></Route>
       </Routes>
       <Footer/>
     </>
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,16 @@
+import './Home.css';
+import imgHome1 from '../../assets/petitimghome4.png'
+export default function Home() {
+  return (
+    <>
+        <div className="bgHome">
+            <div className="brandyDesign">
+                <img src={imgHome1} alt="" />
+            </div>
+            <div className="imgHome">
+
+            </div>
+        </div>
+    </>
+  );
+}
